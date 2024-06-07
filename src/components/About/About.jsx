4793c6191c3fcbeaeb35ca39cb4ecacdfa14a2ea
Parthik Diyora <ddiyora503@gmail.com>
2024-06-07 15:39:53 +0530

    first commit

---
+++ src/components/About/About.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+
+export default function About() {
+  return (
+      <div classNameName="py-16 bg-white">
+          <div classNameName="container m-auto px-6 text-gray-600 md:px-12 xl:px-6">
+              <div classNameName="space-y-6 md:space-y-0 md:flex md:gap-6 lg:items-center lg:gap-12">
+                  <div classNameName="md:5/12 lg:w-5/12">
+                      <img
+                          src="https://tailus.io/sources/blocks/left-image/preview/images/startup.png"
+                          alt="image"
+                      />
+                  </div>
+                  <div classNameName="md:7/12 lg:w-6/12">
+                      <h2 classNameName="text-2xl text-gray-900 font-bold md:text-4xl">
+                          React development is carried out by passionate developers
+                      </h2>
+                      <p classNameName="mt-6 text-gray-600">
+                          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eum omnis voluptatem
+                          accusantium nemo perspiciatis delectus atque autem! Voluptatum tenetur beatae unde
+                          aperiam, repellat expedita consequatur! Officiis id consequatur atque doloremque!
+                      </p>
+                      <p classNameName="mt-4 text-gray-600">
+                          Nobis minus voluptatibus pariatur dignissimos libero quaerat iure expedita at?
+                          Asperiores nemo possimus nesciunt dicta veniam aspernatur quam mollitia.
+                      </p>
+                  </div>
+              </div>
+          </div>
+      </div>
+  );
+}
